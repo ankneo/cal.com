1eaa2b64b239191d06eedff91c3c0276920b38ac
Ankit Jain <ankit@wingify.com>
2022-09-29 15:15:21 +0530

    Add the default demo meeting

--- apps/web/components/getting-started/steps-views/UserProfile.tsx
+++ apps/web/components/getting-started/steps-views/UserProfile.tsx
@@ -81,19 +81,9 @@ const UserProfile = (props: IUserProfile) => {
 
   const DEFAULT_EVENT_TYPES = [
     {
-      title: t("15min_meeting"),
-      slug: "15min",
-      length: 15,
-    },
-    {
-      title: t("30min_meeting"),
-      slug: "30min",
-      length: 30,
-    },
-    {
-      title: t("secret_meeting"),
-      slug: "secret",
-      length: 15,
+      title: t("vwo_demo"),
+      slug: "vwo_demo",
+      length: 45,
       hidden: true,
     },
   ];

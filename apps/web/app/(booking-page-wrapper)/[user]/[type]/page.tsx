ab0ca692d412c2164f631cd3f27f5cf1ed1abae2
vikram koshyari <vikram.koshyari@vwo.com>
2025-05-14 19:22:34 +0530

    track visits of booking page

--- apps/web/app/(booking-page-wrapper)/[user]/[type]/page.tsx
+++ apps/web/app/(booking-page-wrapper)/[user]/[type]/page.tsx
@@ -4,6 +4,8 @@ import { generateMeetingMetadata } from "app/_utils";
 import { headers, cookies } from "next/headers";
 
 import { getOrgFullOrigin } from "@calcom/features/ee/organizations/lib/orgDomains";
+import { RedisService } from "@calcom/features/redis/RedisService";
+import { trackEvent } from "@calcom/lib/analytics";
 
 import { buildLegacyCtx, decodeParams } from "@lib/buildLegacyCtx";
 
@@ -54,6 +56,31 @@ const getData = withAppDirSsr<LegacyPageProps>(getServerSideProps);
 const ServerPage = async ({ params, searchParams }: PageProps) => {
   const legacyCtx = buildLegacyCtx(await headers(), await cookies(), await params, await searchParams);
   const props = await getData(legacyCtx);
+  const redisClient = new RedisService();
+
+  // Get the URL path parameters for user and type
+  const { user, type } = await params;
+
+  // Construct the key for redis
+  const queryParams = await searchParams;
+  const key = `bookingPageVisited|${user}|${type}|${queryParams?.t_source}|${queryParams?.t_account_id}|${queryParams?.t_user_id}`;
+  const isBookingPageVisited = await redisClient.get(key);
+  // Check if the user has not visited the booking page previously in 1 day and if they have, track the event
+  if (
+    !isBookingPageVisited &&
+    (queryParams["t_source"] || queryParams["t_account_id"] || queryParams["t_user_id"])
+  ) {
+    const eventData = {
+      source: queryParams["t_source"],
+      accountId: queryParams["t_account_id"],
+      userId: queryParams["t_user_id"],
+    };
+    trackEvent("SETUP_MEETING_ATTEMPTED", eventData);
+
+    // Set key in redis
+    await redisClient.set(key, 1);
+    await redisClient.expire(key, 86400); // expire in 1 day
+  }
 
   return <LegacyPage {...props} />;
 };

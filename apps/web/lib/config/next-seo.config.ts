9c649ad55189ff45ded732f82802e9beeadaf30a
vikram koshyari <vikram.koshyari@vwo.com>
2026-06-23 14:03:15 +0530

    update meta data

--- apps/web/lib/config/next-seo.config.ts
+++ apps/web/lib/config/next-seo.config.ts
@@ -27,7 +27,7 @@ export const seoConfig: {
   defaultNextSeo: DefaultSeoProps;
 } = {
   headSeo: {
-    siteName: "VWO.com",
+    siteName: "wingify.com",
   },
   defaultNextSeo: {
     twitter: {
